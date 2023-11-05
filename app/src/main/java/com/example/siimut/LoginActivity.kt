dc46f8f4df4bad3d90b319e6530ccb3eac519a6b
iniharbina <harbinaputri4@gmail.com>
2023-11-05 19:21:25 +0700

    Menambahkan Activity Login dan Register

---
+++ app/src/main/java/com/example/siimut/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.siimut
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
