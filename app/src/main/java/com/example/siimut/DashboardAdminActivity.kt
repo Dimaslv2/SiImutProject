7bed213b7bc9a58854a4d494c0fb04224d219565
iniharbina <harbinaputri4@gmail.com>
2023-11-26 17:09:28 +0700

    Menambahkan DashboardAdminActivity

--- app/src/main/java/com/example/siimut/DashboardAdminActivity.kt
+++ app/src/main/java/com/example/siimut/DashboardAdminActivity.kt
@@ -1,11 +1,49 @@
 package com.example.siimut
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.siimut.databinding.ActivityDashboardAdminBinding
 
 class DashboardAdminActivity : AppCompatActivity() {
+
+    //view binding
+    private lateinit var binding: ActivityDashboardAdminBinding
+
+    //firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard_admin)
+        binding = ActivityDashboardAdminBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //init firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        //handle click, logout
+        binding.logoutbtn.setOnClickListener{
+            firebaseAuth.signOut()
+            checkUser()
+        }
+
+        //handle
+    }
+
+    private fun checkUser(){
+        //get current user
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser == nul){
+            //not logged in, goto main screen
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+        else{
+            //logged in, get and show user info
+            val email = firebaseUser.email
+            //set to textview off toolbar
+            binding.subTitleTv.text = email
+        }
     }
 }
