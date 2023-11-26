54a3dddef1f60059bfc1512c17ea52bb9c6dd64a
iniharbina <harbinaputri4@gmail.com>
2023-11-26 17:30:23 +0700

    Menambahkan DashboardUserActivity

--- app/src/main/java/com/example/siimut/DashboardUserActivity.kt
+++ app/src/main/java/com/example/siimut/DashboardUserActivity.kt
@@ -1,11 +1,47 @@
 package com.example.siimut
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.siimut.databinding.ActivityDashboardAdminBinding
+import com.example.siimut.databinding.ActivityDashboardUserBinding
 
 class DashboardUserActivity : AppCompatActivity() {
+
+    //view binding
+    private lateinit var binding: ActivityDashboardUserBinding
+
+    //firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard_user)
+        binding = ActivityDashboardUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+            //init firebase auth
+        firebaseAuth = FirebaseAurh.getInstance()
+        checkUser()
+
+        binding.logoutBtn.setOnClickListener{
+            firebaseAuth.signOut()
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun checkUser(){
+        //get current user
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser == nul){
+            //not logged in, user can stay in user dashboard
+            binding.subTitleTv.text = "Not Logged In"
+        }
+        else{
+            //logged in, get and show user info
+            val email = firebaseUser.email
+            //set to textview off toolbar
+            binding.subTitleTv.text = email
+        }
     }
 }
--- app/src/main/java/com/example/siimut/SplashActivity.kt
+++ app/src/main/java/com/example/siimut/SplashActivity.kt
@@ -8,12 +8,45 @@ import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
 
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed(Runnable {
+            checkUser()
+        },1000)
+    }
+
+    private fun checkUser(){
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser == null){
             startActivity(Intent(this, MainActivity::class.java))
-        },3000)
+            finish()
+        }
+        else{
+            val ref = FirebaseDatabase.getInstance().getReference("Users")
+            ref.child(firebaseUser.uid)
+                .addListenerForSingleValueEvent(object : ValueEventListener{
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        progressDialog.dismiss()
+                        val userType = snapshot.child("userType").value
+                        if (userType == "user"){
+                            //user
+                            startActivity(Intent(this@SplashActivity, DashboardUserActivity::class.java))
+                            finish()
+                        }
+                        else if (userType == "admin"){
+                            //admin
+                            startActivity(Intent(this@SplashActivity, DashboardAdminActivity::class.java))
+                            finish()
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+
+                    }
+                })
+        }
     }
 }
